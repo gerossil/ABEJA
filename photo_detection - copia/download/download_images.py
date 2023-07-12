6dca8ff8ba6148b1de8766d17f2ab6b988d17535
GEROSSIER LEO <leo.gerossier@cpe.fr>
2023-07-12 09:49:13 -0500

    wefew

--- photo_detection - copia/download/download_images.py
+++
@@ -1,8 +0,0 @@
-from simple_image_download import simple_image_download as simp
-
-response = simp.simple_image_download
-
-keywords = ["solitary bee hives complete"]
-
-for keyword in keywords:    
-    response().download(keyword, 100)
