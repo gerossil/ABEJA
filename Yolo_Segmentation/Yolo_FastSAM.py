91ae468d8f303d7f07b7295fe744754175f84687
Leo Gerossier <leo.gerossier38@gmail.com>
2023-08-18 12:13:31 -0500

    deletion

--- Yolo_Segmentation/Yolo_FastSAM.py
+++
@@ -1,5 +0,0 @@
-
-
-from ultralytics.yolo.data.annotator import auto_annotate
-
-auto_annotate(data="C:\\Users\\LABSIS\\Documents\\ABEJA\\Yolo_Segmentation\\Hole_33.png", det_model="yolov8x.pt", sam_model='sam_b.pt')
